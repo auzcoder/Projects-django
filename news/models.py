fab50974b138518c835052ce7f523b92dd97426d
AuzCoder <auz.offical@gmail.com>
2023-02-07 15:12:20 +0500

    1
    1111111111111

--- news/models.py
+++ news/models.py
@@ -13,6 +13,7 @@ class New (models.Model):
     full_info =RichTextField('text', config_name='extends')
     header_images = models.ImageField(default='news.png', blank=True)
     date = models.DateField(default=datetime.now, blank=True)
+   
 
     # userid = models.ForeignKey(User, on_delete=models.CASCADE)
     def __str__(self):
--- news/views.py
+++ news/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import New
 
 
@@ -8,3 +8,9 @@ class PostListView(ListView):
     model = New
     template_name = 'news/news.html'
     context_object_name = 'news'
+
+
+class PostDetailView(DetailView):
+    model = New
+    template_name = 'news/news_detail.html'
+    context_object_name = 'post'
