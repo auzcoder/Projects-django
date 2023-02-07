14b5a0fe913c79b5516105dad445d718c2ca7c21
AuzCoder <auz.offical@gmail.com>
2023-02-07 15:12:20 +0500

    YAngilanishlar qo'shildi...

--- news/urls.py
+++ news/urls.py
@@ -1,8 +1,11 @@
 from django.conf import settings
 from django.urls import path
 from django.conf.urls.static import static
-from .views import PostListView
+from .views import PostListView, PostDetailView
 
 urlpatterns = [
     path('news/', PostListView.as_view(), name = 'post_list'),
-]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('news/<int:id>/', PostDetailView.as_view(), name = 'post_detail'),
+]
+
+
