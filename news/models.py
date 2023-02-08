ede520ac16a8d222d112c6dbbd75341a94978c5a
AuzCoder <auz.offical@gmail.com>
2023-02-08 15:43:35 +0500

    Yangilandi...
    Yangilandi...

--- news/models.py
+++ news/models.py
@@ -1,18 +1,32 @@
-from datetime import datetime
+
 from django.contrib.auth.models import User
 from django.db import models
 from ckeditor.fields import RichTextField
+from django.utils import timezone
 
 
 # Create your models here.
 
 # Yangiliklar uchun postlar qismi
 class New (models.Model):
+    class Status(models.TextChoices):
+        Draft = 'DF', 'Draft'
+        Published = 'PB', 'Published'
+
     name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
     description = models.CharField(max_length=200, blank=True)
-    full_info =RichTextField('text', config_name='extends')
-    header_images = models.ImageField(default='news.png', blank=True)
-    date = models.DateField(default=datetime.now, blank=True)
+    full_info =RichTextField('text')
+    header_images = models.ImageField(default='news.png', upload_to = 'uploads/news/images', blank=True)
+    category = models.ForeignKey()
+    #upload_to = 'news/image'
+    date = models.DateTimeFieldField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2,
+                              choices=Status.choices,
+                              default=Status.Draft
+                               )
    
 
     # userid = models.ForeignKey(User, on_delete=models.CASCADE)
