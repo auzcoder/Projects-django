bbe7cc13dbdeb1ca9858ea1f648c535f153fff59
AuzCoder <auz.offical@gmail.com>
2023-02-17 11:53:26 +0500

    ...
    ...

--- news/custom_context_processor.py
+++ news/custom_context_processor.py
@@ -1,7 +1,8 @@
-from .models import New
+from .models import New, Category
 
 def news_list(request):
     news_list = New.published.all().order_by('-date')[:5]
+    categories = Category.objects.all()
 
     context = {
         'news_list': news_list,
