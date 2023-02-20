dba487aaa77485e819f3ea3cf2f775513c2f92ea
AuzCoder <auz.offical@gmail.com>
2023-02-20 16:06:23 +0500

    login
    login

--- accounts/views.py
+++ accounts/views.py
@@ -4,3 +4,18 @@ from django.contrib.auth import authenticate, login, logout
 from .forms import LoginForm
 
 # Create your views here.
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            user = authenticate(request,
+                                username=data['username'],
+                                password=data['password']
+                                )
+            if user is not None:
+                if user.is_active():
+                    login(request, user)
+                    return HttpResponse('Siz tizimga kirdingiz!')
+                else:
+                    return HttpResponse('Sizni hissobingiz faol holatda emas!')
