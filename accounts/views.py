1ab1d564190a2e0750424fc80d6ee9f7e0fb8674
AuzCoder <auz.offical@gmail.com>
2023-02-22 17:08:15 +0500

    ..
    ..

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,11 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse, reverse_lazy
 from django.views.generic import ListView
 
+import news
 from .forms import LoginForm
 
 # Create your views here.
@@ -19,7 +21,8 @@ def user_login(request):
             if user is not None:
                 if user.is_active:
                     login(request, user)
-                    return HttpResponse('Siz tizimga kirdingiz!')
+                    success_url = 'admin_home_page'
+                    return redirect(success_url)
                 else:
                     return HttpResponse('Sizni hissobingiz faol holatda emas!')
             else:
@@ -36,6 +39,7 @@ def user_login(request):
 
 
 class AdminHomePageView(ListView):
+    model = User
     template_name = 'admin/home.html'
     context_object_name = 'admin_home_page'
 
