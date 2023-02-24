e08dd1bb48d53db51c9156c0b1cd934b028fed4c
AuzCoder <auz.offical@gmail.com>
2023-02-24 10:27:08 +0500

    changepass
    changepass

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,6 @@ from django.contrib.auth.views import PasswordChangeView, PasswordChangeDoneView
 urlpatterns = [
     path('home/', AdminHomePageView.as_view(), name='admin_home_page'),
     path('login/', user_login, name='auth_login'),
-    path('pass-change/', PasswordChangeView.as_view(), name='password_change'),
+    path('password-change/', PasswordChangeView.as_view(), name='password_change'),
     path('pass-change-done/', PasswordChangeDoneView.as_view(), name='password_change_done'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,7 @@
 from django.contrib.auth.models import User
+from django.contrib.auth import update_session_auth_hash
+
+from django.core.checks import messages
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth import authenticate, login, logout
@@ -43,3 +46,47 @@ class AdminHomePageView(ListView):
     template_name = 'admin/home.html'
     context_object_name = 'admin_home_page'
 
+
+
+
+def change_password(request):
+    if request.method == 'POST':
+        old_password = request.POST.get("password_old")
+        new_password = request.POST.get("password_new1")
+        confirmed_new_password = request.POST.get("password_new2")
+
+        if old_password and new_password and confirmed_new_password:
+            if request.user.is_authenticated:
+                user = User.objects.get(username=request.user.username)
+                if not user.check_password(old_password):
+                    messages.warning(request, "your old password is not correct!")
+                else:
+                    if new_password != confirmed_new_password:
+                        messages.warning(request, "your new password not match the confirm password !")
+
+                    elif len(new_password) < 8 or new_password.lower() == new_password or \
+                            new_password.upper() == new_password or new_password.isalnum() or \
+                            not any(i.isdigit() for i in new_password):
+                        messages.warning(request, "your password is too weak!")
+
+
+
+                    else:
+                        user.set_password(new_password)
+                        user.save()
+                        update_session_auth_hash(request, user)
+
+                        messages.success(request, "your password has been changed successfuly.!")
+
+                        return redirect('dashboard_namespace:home')
+
+        else:
+            messages.warning(request, " sorry , all fields are required !")
+
+    context = {
+
+    }
+    return render(request, "admin/reset-password.html", context)
+
+
+i
