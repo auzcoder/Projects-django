6876b4b8e6228ee942eb5f6eb123d88f07770cce
AuzCoder <auz.offical@gmail.com>
2023-02-27 19:03:26 +0500

    ..
    ..

--- news/urls.py
+++ news/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import PostListView, PostDetailView, NewsUpdateView, NewsDeleteView
 
+# urls
+
 urlpatterns = [
     path('', PostListView.as_view(), name='post_list'),
     path('<slug:slug>/', PostDetailView.as_view(), name='post_detail'),
