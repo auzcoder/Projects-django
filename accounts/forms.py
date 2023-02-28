41ac9ac6b813bb1044fb7881c2de20781910e47a
AuzCoder <auz.offical@gmail.com>
2023-02-28 14:30:39 +0500

    ..
    ..

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,12 @@
 from django import forms
+from news.models import New
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+class AdminNewsUpdateView(forms.Form):
+    name = forms.CharField(max_length=200)
+    description = forms.CharField(max_length=200)
+
+
