0487024c87eba4fc4d0cfd6367c51542736b45b6
AuzCoder <auz.offical@gmail.com>
2023-02-28 12:52:20 +0500

    ..
    ..

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from accounts.views import user_login, AdminHomePageView, change_password, logout, admin_news_list, AdminNewsUpdateView
+from accounts.views import user_login, AdminHomePageView, change_password, logout, admin_news_list, AdminNewsUpdateView, AdminPostDetailView
 from django.contrib.auth.views import PasswordChangeView, PasswordChangeDoneView
 
 urlpatterns = [
@@ -10,6 +10,6 @@ urlpatterns = [
     path('pass-change-done/', PasswordChangeDoneView.as_view(), name='password_change_done'),
     # path('pass-reset/', reset_password, name='reset_password')
     path('list/', admin_news_list, name='admin_news_list'),
-    path('list/detail/<slug:slug>/'),
+    path('list/detail/<slug:slug>/', AdminPostDetailView.as_view(), name='admin_news_detail'),
     path('list/edit/<slug:slug>/', AdminNewsUpdateView.as_view(), name='admin_news_update')
 ]
