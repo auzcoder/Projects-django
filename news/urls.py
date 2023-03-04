9d7d23c280880743b6a7245dd87bc3f3968d3c2b
AuzCoder <auz.offical@gmail.com>
2023-03-04 11:06:51 +0500

    ..
    ..

--- news/urls.py
+++ news/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path('<slug:slug>/', PostDetailView.as_view(), name='post_detail'),
     path('edit/<slug:slug>/', NewsUpdateView.as_view(), name='post_edit'),
     path('delete/<slug:slug>/', NewsDeleteView.as_view(), name='post_delete'),
-    # path('search/', NewsSearchView.as_view(), name='search_news'),
 ]
