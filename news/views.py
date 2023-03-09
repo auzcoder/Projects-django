9fe17cd02338192c5a8bce093de330cd88da5d1b
AuzCoder <auz.offical@gmail.com>
2023-03-09 15:52:41 +0500

    Update views.py

--- news/views.py
+++ news/views.py
@@ -102,51 +102,6 @@ class PostDetailView(HitCountDetailView):
     context_object_name = 'news'
     count_hit = True
 
-#
-# class PostCountHitDetailView(HitCountDetailView):
-#     model = New  # your model goes here
-#     count_hit = True
-#
-#     def some_view(request, slug):
-#         object = get_object_or_404(New, slug=slug)
-#         context = {}
-#
-#         # hitcount logic
-#         hit_count = get_hitcount_model().objects.get_for_object(object)
-#         hits = hit_count.hits
-#         hitcontext = context['hitcount'] = {'slug': hit_count.slug}
-#         hit_count_response = HitCountMixin.hit_count(request, hit_count)
-#         if hit_count_response.hit_counted:
-#             hits = hits + 1
-#             hitcontext['hit_counted'] = hit_count_response.hit_counted
-#             hitcontext['hit_message'] = hit_count_response.hit_message
-#             hitcontext['total_hits'] = hits
-#
-#         # … extra logic …
-#
-#         return render(request, 'news/news_detail.html', context)
-
-        # hit_count = get_hitcount_model().object.get_for_object(New)
-    # hits = hit_count.hits
-    # hitcontext =context_object_name ['hitcount'] = {'slug': hit_count.slug}
-    # hit_count_response = HitCountMixin.hit_count(request, hit_count)
-    # if hit_count_response.hit_counted:
-    #     hits = hits + 1
-    #     hitcontext['hit_counted'] = hit_count_response.hit_counted
-    #     hitcontext['hit_message'] = hit_count_response.hit_message
-    #     hitcontext['total_hits'] = hits
-
-
-
-    # # Ko'rishlar sonini xissoblash uchun'
-    # def get_object(self):
-    #     post = get_object_or_404(New, slug=self.kwargs['slug'])
-    #
-    #     if post:
-    #         post.view_count = post.view_count + 1
-    #         post.save()
-    #     return post
-
 
 
 # # Ko'rishlar uchun viewslar sonini aniqlash
