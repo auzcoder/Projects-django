bf9c80a11f8e858fe01da523289f71d3529e2ac9
AuzCoder <auz.offical@gmail.com>
2023-03-11 20:14:00 +0500

    .

--- MyNews/settings.py
+++ MyNews/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 from django.utils.translation import gettext_lazy as _
 
 LANGUAGES = [
-    ('uz', _('Uzbek')),
+    ('uz', _('O\'zbekcha')),
     ('en', _('English')),
     ('ru', _('Russian')),
 ]
