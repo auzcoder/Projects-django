4bc401c35d4d4bce9cfc890d61377f5fe28d133d
AuzCoder <auz.offical@gmail.com>
2023-03-11 13:19:11 +0500

    .

--- MyNews/urls.py
+++ MyNews/urls.py
@@ -1,4 +1,4 @@
-
+from django.conf.urls.i18n import i18n_patterns
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -9,12 +9,16 @@ from news.views import HomePageView, ContactPageView, NewsCreateView, NewsSearch
 urlpatterns = [
     path('auth/', include('accounts.urls')),
     path('admin/', admin.site.urls, name='admin'),
+
+] + i18n_patterns(
+    path('i18n/', include('django.conf.urls.i18n')),
     path('', HomePageView, name='home'),
     path('news/', include('news.urls')),
     path('contact/', ContactPageView.as_view(), name='contact'),
     path('create/', NewsCreateView.as_view(), name='create_news'),
     path('search/', NewsSearchView.as_view(), name='search_news'),
-]
+)
 
 if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
